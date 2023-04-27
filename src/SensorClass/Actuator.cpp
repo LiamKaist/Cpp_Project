c094c203e0483c156310be4245c059409e54b40d
LiamKaist <117256858+LiamKaist@users.noreply.github.com>
2023-04-27 19:17:16 +0200

    Delete Actuator.cpp

--- src/SensorClass/Actuator.cpp
+++
@@ -1,8 +0,0 @@
-#include "Actuator.hpp"
-#include <iostream>
-
-int Actuator::actuatorNbr=0;
-
-Actuator::Actuator(){ //Definition du code à l'interieur du constructeur
-    std::cout<< "In Actuator constructor" << std::endl;
-}
