2c43370e2983654b205d77c1dcfac1db32596a98
LiamKaist <117256858+LiamKaist@users.noreply.github.com>
2023-04-27 19:18:16 +0200

    Add files via upload

---
+++ src/ActuatorClass/Actuator.cpp
@@ -0,0 +1,8 @@
+#include "Actuator.hpp"
+#include <iostream>
+
+int Actuator::actuatorNbr=0;
+
+Actuator::Actuator(){ //Definition du code à l'interieur du constructeur
+    std::cout<< "In Actuator constructor" << std::endl;
+}
