b1ae6724f14453b60b8bf6cfef65b054879ad87f
LiamKaist <117256858+LiamKaist@users.noreply.github.com>
2023-04-27 19:19:31 +0200

    Add files via upload

---
+++ src/PumpController/Pump.cpp
@@ -0,0 +1,43 @@
+#include "Pump.hpp"
+
+
+
+#include <Arduino.h>
+#include <iostream>
+
+
+Pump::Pump(){
+
+}
+
+
+//Instantiate the pump with its pin , set to OUTPUT Mode
+Pump::Pump(uint8_t pin){
+    pinMode(pin,OUTPUT);
+    this->pin= pin;
+    this->previousMillis=0UL;
+    this->interval = 7250UL;
+}
+
+void Pump::sendSignal(){
+
+    std::cout<<"In sendSignal()";
+    digitalWrite(this->pin,HIGH);
+
+    this->previousMillis=millis();
+    while(millis() < this->previousMillis + this->interval){yield();} //Yield is used to reset the watchdog timer, it could be possible to change the watchdog timer's threshold}
+
+
+    digitalWrite(this->pin,LOW);
+    
+    //More timing precision using millis()
+    this->previousMillis=millis();
+    while(millis() < this->previousMillis + this->interval){yield();}
+
+}
+
+void Pump::activatePump(){
+
+    std::cout<<"In activatePump()"; 
+
+}
