df0b8e9b52e8f58c562e641ab28481e39884ac0b
LiamKaist <117256858+LiamKaist@users.noreply.github.com>
2023-05-13 00:06:20 +0200

    Update MP4Translate.sh

--- src/MP4Transfer/MP4Translate.sh
+++ src/MP4Transfer/MP4Translate.sh
@@ -15,8 +15,8 @@ whisper /home/lilultime/Cpp_Project/src/MP4Transfer/MyPhoneFile/LiamVoiceRecordi
 #Use the text to question ChatGPT
 python3 /home/lilultime/Cpp_Project/src/ChatGPTLink/makeRequest.py /home/lilultime/Cpp_Project/src/MP4Transfer/file.txt > /home/lilultime/Cpp_Project/src/MP4Transfer/Finalfile.txt
 
-#Open a server on the computer on port 8080
-./../CustomHTTPserver/server_linux 
+#Open two servers on the computer on port 8080 and 8081
+./../CustomHTTPserver/server_linux 8080 8081
 
 
 
