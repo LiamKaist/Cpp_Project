bfa8e2239a52f982cac3f813377f7767a9ffa809
LiamKaist <117256858+LiamKaist@users.noreply.github.com>
2023-05-13 17:40:03 +0200

    Add files via upload

--- src/CustomOled/Oled.cpp
+++ src/CustomOled/Oled.cpp
@@ -1,28 +1,59 @@
 
 
-#include "../U8g2/src/U8g2lib.h"
+#include <Oled.hpp>
 
+using namespace std;
+Oled::Oled(const u8g2_cb_t *r,uint8_t clockie,uint8_t data,uint8_t reset): U8G2_SH1107_SEEED_128X128_1_SW_I2C(r,clockie,data,reset)
+{this->initOK=1;}
 
-class Oled: public U8G2_SH1107_SEEED_128X128_1_SW_I2C {
+void Oled::diagDraw(void){
+    
+    Oled::drawBox(1,1,10,10);
+    Oled::drawBox(11,11,10,10);
+    Oled::drawBox(21,21,10,10);
+    Oled::drawBox(31,31,10,10);
+    Oled::drawBox(41,41,10,10);
+    Oled::drawBox(51,51,10,10);
+    Oled::drawBox(61,61,10,10);
 
-    protected:
-    int initOK = 0;
+}
 
+void Oled::sentenceWrite(string sentence, int maxLen){
+    sentence=Oled::removeSpaces(sentence);
 
-    public:
+    list<string> sentenceList;
+    //Use of STL list library to display on Oled
+    int i;
+    for (i=0; i<sentence.size(); i+=maxLen){
 
-        Oled(const u8g2_cb_t *r,uint8_t clockie,uint8_t data,uint8_t reset): U8G2_SH1107_SEEED_128X128_1_SW_I2C(r,clockie,data,reset)
-        {this->initOK=1;}
-
-        void diagDraw(void){
-            
-            U8G2_SH1107_SEEED_128X128_1_SW_I2C::drawBox(1,1,10,10);
-            U8G2_SH1107_SEEED_128X128_1_SW_I2C::drawBox(11,11,10,10);
-            U8G2_SH1107_SEEED_128X128_1_SW_I2C::drawBox(21,21,10,10);
-            U8G2_SH1107_SEEED_128X128_1_SW_I2C::drawBox(31,31,10,10);
-            U8G2_SH1107_SEEED_128X128_1_SW_I2C::drawBox(41,41,10,10);
-            U8G2_SH1107_SEEED_128X128_1_SW_I2C::drawBox(51,51,10,10);
-            U8G2_SH1107_SEEED_128X128_1_SW_I2C::drawBox(61,61,10,10);
-        
+        if ((i+maxLen < sentence.size()) && (sentence[i+maxLen+1]!=' ')){
+            sentenceList.push_back(sentence.substr(i, maxLen));
         }
-};
+        else{
+            sentenceList.push_back(sentence.substr(i));
+        }
+
+    }
+
+    list<string>::iterator it;
+    for (it = sentenceList.begin(); it != sentenceList.end(); ++it){
+        Oled::firstPage();
+        do {
+            Oled::setFont(u8g2_font_ncenB10_tr);
+            Oled::drawStr(0,24, (*it).c_str());
+        } while ( Oled::nextPage() );
+        delay(1000);
+    }
+}
+
+string Oled::removeSpaces(string sentence){
+    sentence.erase(std::remove_if(sentence.begin(), sentence.end(), ::isspace),sentence.end()); //Removing spaces to prevent display issues
+    return sentence;
+}
+
+
+
+void Oled::operator<< (const string & sentence)
+{
+    Oled::sentenceWrite(sentence,10);
+}
--- src/CustomOled/Oled.hpp
+++ src/CustomOled/Oled.hpp
@@ -1,29 +1,25 @@
+#ifndef OLED_H
+#define OLED_H
 
 
+#include <U8g2lib.h>
+#include <list>
+#include <string>
 
-#include "../U8g2/src/U8g2lib.h"
 
-
+using namespace std;
 class Oled: public U8G2_SH1107_SEEED_128X128_1_SW_I2C {
 
     protected:
     int initOK = 0;
-
+    string removeSpaces(string sentence);
 
     public:
+    Oled(const u8g2_cb_t *r,uint8_t clockie,uint8_t data,uint8_t reset);
+    void diagDraw(void);
+    void sentenceWrite(string sentence, int maxLen);
+    void operator << (const string & sentence);
 
-        Oled(const u8g2_cb_t *r,uint8_t clockie,uint8_t data,uint8_t reset): U8G2_SH1107_SEEED_128X128_1_SW_I2C(r,clockie,data,reset)
-        {this->initOK=1;}
-
-        void diagDraw(void){
-            
-            U8G2_SH1107_SEEED_128X128_1_SW_I2C::drawBox(1,1,10,10);
-            U8G2_SH1107_SEEED_128X128_1_SW_I2C::drawBox(11,11,10,10);
-            U8G2_SH1107_SEEED_128X128_1_SW_I2C::drawBox(21,21,10,10);
-            U8G2_SH1107_SEEED_128X128_1_SW_I2C::drawBox(31,31,10,10);
-            U8G2_SH1107_SEEED_128X128_1_SW_I2C::drawBox(41,41,10,10);
-            U8G2_SH1107_SEEED_128X128_1_SW_I2C::drawBox(51,51,10,10);
-            U8G2_SH1107_SEEED_128X128_1_SW_I2C::drawBox(61,61,10,10);
-        
-        }
 };
+
+#endif
